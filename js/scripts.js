8359ed1f5753960f6ea7b612ed5fede71b654a1b
rochelleyuan <rochelleyuan@gmail.com>
2021-03-26 18:15:13 -0700

    add mobile menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("welcome to my portfolio")
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
