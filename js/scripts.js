907979e7d3c48965e2e548ed1ed6583c664bcf0f
rochelleyuan <rochelleyuan@gmail.com>
2021-04-07 01:00:05 -0700

    updated about page styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-console.log("Hi, welcome to my portfolio!")
+console.log('Hi, welcome to my portfolio!')
 
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
